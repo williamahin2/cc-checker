7b1dc17c365a566109bd37eedf406c28e2788fba
williamahin2 <75301087+williamahin2@users.noreply.github.com>
2020-12-14 15:40:11 +0530

    Update CCGen.php

--- CCGen.php
+++ CCGen.php
@@ -146,7 +146,8 @@ body {
 <div class="topnav" id="myTopnav">
     <span class="topnavhead" id="demo"><b><img alt="Image placeholder" style="
     display: inline-block;margin-right: 5px; margin-left: 1px;padding-top: 0px;padding-bottom: 0px; height: 40px; width: 43px;" src="logo.png"> CrossFire Checker</b></span>
-      <a href="skcheck.php" >SK Checker</a>
+      <a href="index.php" >CC Checker</a>
+	<a href="skcheck.php" >SK Checker</a>
         <a href="skgen.php">SK Generator</a>
           <a href="CCGen.php" class="topnavac" style="color: white">CC Generator</a>
                     
